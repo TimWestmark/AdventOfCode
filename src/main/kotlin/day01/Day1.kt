9fb8a68a7b13880c953405afa53a38b088f10284
Tim Westmark <tim.westmark@arvato.com>
2022-11-28 10:46:33 +0100

    Initial Commit

---
+++ src/main/kotlin/day01/Day1.kt
@@ -0,0 +1,15 @@
+package day01
+
+fun main(args: Array<String>) {
+    println(part1())
+    println(part2())
+}
+
+fun part1(): String {
+    return "Solution Part 1"
+}
+
+
+fun part2(): String {
+    return "Solution Part 2"
+}
