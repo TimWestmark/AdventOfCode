fbded0fdc9bc695a1dcf9e152742eca5477a4d3a
Tim Westmark <westmark.tim@gmail.com>
2022-12-01 18:26:00 +0100

    Small template and Day1 improvements

--- src/main/kotlin/day01/Day1.kt
+++ src/main/kotlin/day01/Day1.kt
@@ -3,8 +3,8 @@ package day01
 import getInputLines
 
 fun main() {
-    println(part1())
-    println(part2())
+    println("Solution Part 1: ${part1()}")
+    println("Solution Part 1: ${part2()}")
 }
 
 fun input(): List<Int> {
@@ -25,12 +25,15 @@ fun input(): List<Int> {
 
 }
 
-fun part1(): Int {
-    return input().max()
+fun part1(): Int? {
+    return input().maxOrNull()
 }
 
-fun part2(): Int {
+fun part2(): Int? {
+
     val lastIndex = input().lastIndex
+    if (lastIndex <= 2) return null
+
     val sortedList = input().sorted()
     return sortedList[lastIndex] + sortedList[lastIndex - 1] + sortedList[lastIndex - 2]
 }
--- src/main/kotlin/template/DailyTemplate.kt
+++ src/main/kotlin/template/DailyTemplate.kt
@@ -1,16 +1,15 @@
 package template
 
 fun main() {
-    println("Solution Part 1: " + part1())
-    println("Solution Part 2: " + part2())
-    println(part2())
+    println("Solution Part 1: ${part1()}")
+    println("Solution Part 1: ${part2()}")
 }
 
-fun part1(): Int {
+fun part1(): Int? {
     return 1
 }
 
 
-fun part2(): Int {
+fun part2(): Int? {
     return 2
 }
