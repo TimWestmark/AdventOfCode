2fe82eb4756dab34fc62c58140b7e0c7931ce38b
Tim Westmark <tim.westmark@arvato.com>
2022-12-01 08:08:35 +0100

    Solve day 1

--- src/main/kotlin/day01/Day1.kt
+++ src/main/kotlin/day01/Day1.kt
@@ -1,15 +1,36 @@
 package day01
 
-fun main(args: Array<String>) {
+import getInputLines
+
+fun main() {
     println(part1())
     println(part2())
 }
 
-fun part1(): String {
-    return "Solution Part 1"
+fun input(): List<Int> {
+    val weights: MutableList<Int> = mutableListOf()
+
+    var curElvesWeight = 0
+//    getInputLines("/day1/test-input.txt")
+    getInputLines("/day1/input.txt").forEach { line ->
+            if (line.isBlank()) {
+                weights.add(curElvesWeight)
+                curElvesWeight = 0
+            } else {
+                curElvesWeight += Integer.valueOf(line)
+            }
+        }
+
+    return weights.toList()
+
 }
 
+fun part1(): Int {
+    return input().max()
+}
 
-fun part2(): String {
-    return "Solution Part 2"
+fun part2(): Int {
+    val lastIndex = input().lastIndex
+    val sortedList = input().sorted()
+    return sortedList[lastIndex] + sortedList[lastIndex - 1] + sortedList[lastIndex - 2]
 }
