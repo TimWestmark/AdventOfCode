692bc3bc553a9571279b9b90c83dfac225b0bb95
Tim Westmark <westmark.tim@gmail.com>
2022-12-07 14:27:01 +0100

    Solve day 7

---
+++ src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,85 @@
+package day07
+
+import getInputLines
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    val timeInMillisPart1 = measureTimeMillis {
+        println("Solution Part 1: ${part1()}")
+    }
+
+    val timeInMillisPart2 = measureTimeMillis {
+        println("Solution Part 2: ${part2()}")
+    }
+
+    println("(The part1 operation took $timeInMillisPart1 ms)")
+    println("(The part2 operation took $timeInMillisPart2 ms)")
+}
+
+data class Directory(
+    val name: String,
+    val subDirs: MutableList<Directory>,
+    var parentDir: Directory?,
+    var size: Int
+)
+
+fun input(): Directory {
+    val root = Directory("/", mutableListOf(), null, 0)
+    var currentDir = root
+    getInputLines("/day07/input.txt")
+//    getInputLines("/day07/test-input.txt")
+        .forEach { line ->
+            when {
+                line.startsWith("$ cd /") || line == "$ ls" -> {}
+                line.startsWith("$ cd ..") -> currentDir = currentDir.parentDir!!
+                line.startsWith("$ cd ") -> currentDir = currentDir.subDirs.find { dir -> dir.name == line.split(" ")[2] }!!
+                line.startsWith("dir") -> currentDir.subDirs.add(
+                    Directory(
+                        line.split(" ")[1],
+                        mutableListOf(),
+                        currentDir,
+                        0
+                    )
+                )
+                else -> currentDir.addFilesize(line.split(" ")[0].toInt())
+            }
+        }
+
+    return root
+}
+
+fun Directory.addFilesize(size: Int) {
+    this.size += size
+    if (parentDir != null) {
+        this.parentDir!!.addFilesize(size)
+    }
+}
+
+fun getDirSizes(dir: Directory, resultList: MutableList<Int>) {
+    resultList.add(dir.size)
+    dir.subDirs.forEach {
+        getDirSizes(it, resultList)
+    }
+}
+
+fun part1(): Int {
+    val root = input()
+    val flatDirSizes = mutableListOf<Int>()
+    getDirSizes(root, flatDirSizes)
+    return flatDirSizes.filter { dirSize -> dirSize <= 100000 }.sum()
+}
+
+
+fun part2(): Int {
+    val root = input()
+
+    val totalSpace = 70_000_000
+    val freeSpaceNeeded = 30_000_000
+    val unusedSpace = totalSpace - root.size
+    val spaceToBeDeleted = freeSpaceNeeded - unusedSpace
+
+    val flatDirSizes = mutableListOf<Int>()
+    getDirSizes(root, flatDirSizes)
+
+    return flatDirSizes.filter { dirSize -> dirSize >= spaceToBeDeleted }.minOf{ it }
+}
