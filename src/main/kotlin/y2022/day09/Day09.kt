a619d5d74b4eec8fe975e421d89ed12d4ccf5b25
Tim Westmark <tim.westmark@arvato.com>
2022-12-09 08:42:56 +0100

    Prettify Day09 solution

--- src/main/kotlin/y2022/day09/Day09.kt
+++ src/main/kotlin/y2022/day09/Day09.kt
@@ -43,116 +43,78 @@ data class Field(
     val y: Int
 )
 
+fun initField(size:Int): List<List<Field>> {
+    return  IntRange(0, size).map { y ->
+        IntRange(0, size)
+            .map { x ->
+                Field(false, x, y)
 
-fun List<List<Field>>.print(head: Field, tail: Field, start: Field) {
-    this.forEach { line ->
-        line.forEach { cell ->
-
-            when {
-                cell.x == head.x && cell.y == head.y -> print("H")
-                cell.x == tail.x && cell.y == tail.y -> print("T")
-                cell.x == start.x && cell.y == start.y -> print("s")
-                else -> print(".")
             }
-        }
-
-        println()
     }
 }
 
-fun List<List<Field>>.print2(head: Field, tails: List<Field>, start: Field) {
-    this.forEach { line ->
-        line.forEach { cell ->
+fun moveHead(head: Field, direction: Direction, field: List<List<Field>>): Field {
+    return when (direction) {
+        Direction.LEFT -> field[head.y][head.x - 1]
+        Direction.RIGHT -> field[head.y][head.x + 1]
+        Direction.UP -> field[head.y - 1][head.x]
+        Direction.DOWN -> field[head.y + 1][head.x]
+    }
+}
 
-            when {
-                cell.x == head.x && cell.y == head.y -> print("H")
-                tails.indexOf(cell) > -1 -> print(tails.indexOf(cell)+1)
-                cell.x == start.x && cell.y == start.y -> print("s")
-                else -> print(".")
-            }
-        }
+fun moveTail(head:Field, tail: Field, field:List<List<Field>>): Field {
+    return when {
+        // Move the tail horizontally
+        tail.y == head.y && abs(tail.x - head.x) > 1 -> field[tail.y][(tail.x + head.x) / 2]
+        // Move the tail vertically
+        tail.x == head.x && abs(tail.y - head.y) > 1 -> field[(tail.y + head.y) / 2][tail.x]
 
-        println()
+        // move diagonally
+        abs(tail.y - head.y) > 1 && abs(tail.x - head.x) > 1 -> field[(tail.y + head.y) / 2][(tail.x + head.x) / 2]
+        abs(tail.y - head.y) > 1 -> field[(tail.y + head.y) / 2][head.x]
+        abs(tail.x - head.x) > 1 -> field[head.y][(tail.x + head.x) / 2]
+        // Do not move
+        else -> return tail
     }
 }
 
 fun part1(): Int {
 
     val commands = input()
+    val fieldSize = 1000
 
+    val field = initField(fieldSize)
+    val start = field[fieldSize/2][fieldSize/2]
 
-    val field = IntRange(0, 1000).map { y ->
-        IntRange(0, 1000)
-            .map { x ->
-                Field(false, x, y)
-
-            }
-    }
-
-
-    val start = field[500][500]
     start.visitedByTail = true
     var head = start
     var tail = start
 
     commands.forEach { command ->
-
-//        println("${command.direction} ${command.steps}")
         repeat(command.steps) {
-
             // Move the Head
-            head = when (command.direction) {
-                Direction.LEFT -> field[head.y][head.x - 1]
-                Direction.RIGHT -> field[head.y][head.x + 1]
-                Direction.UP -> field[head.y - 1][head.x]
-                Direction.DOWN -> field[head.y + 1][head.x]
-            }
-
-            when {
-                // Move the tail horizontally
-                tail.y == head.y && abs(tail.x - head.x) > 1 -> tail = field[tail.y][(tail.x + head.x) / 2]
-                // Move the tail vertically
-                tail.x == head.x && abs(tail.y - head.y) > 1 -> tail = field[(tail.y + head.y) / 2][tail.x]
-
-                // move diagonally
-                (abs(tail.y - head.y) > 1 || abs(tail.x - head.x) > 1) -> {
-                    when (command.direction) {
-                        Direction.UP, Direction.DOWN -> tail = field[(tail.y + head.y) / 2][head.x]
-                        Direction.LEFT, Direction.RIGHT -> tail =  field[head.y][(tail.x + head.x) / 2]
-                    }
-                }
-            }
-
-
+            head = moveHead(head, command.direction, field)
+            // Move the Tail
+            tail = moveTail(head, tail, field)
 
             tail.visitedByTail = true
-//            field.print(head, tail, start)
-//            println("")
-//            println("")
         }
     }
 
     return field.flatten().count { it.visitedByTail }
 }
 
-
 fun part2(): Int {
 
     val commands = input()
+    val fieldSize = 1000
 
+    val field = initField(fieldSize)
+    val start = field[fieldSize/2][fieldSize/2]
 
-    val field = IntRange(0, 1000).map { y ->
-        IntRange(0, 1000)
-            .map { x ->
-                Field(false, x, y)
-
-            }
-    }
-
-
-    val start = field[500][500]
     start.visitedByTail = true
     var head = start
+
     val tails: MutableList<Field> = mutableListOf(
         start,
         start,
@@ -166,53 +128,22 @@ fun part2(): Int {
     )
 
     commands.forEach { command ->
-
-        println("${command.direction} ${command.steps}")
         repeat(command.steps) {
-
             // Move the Head
-            head = when (command.direction) {
-                Direction.LEFT -> field[head.y][head.x - 1]
-                Direction.RIGHT -> field[head.y][head.x + 1]
-                Direction.UP -> field[head.y - 1][head.x]
-                Direction.DOWN -> field[head.y + 1][head.x]
-            }
-
+            head = moveHead(head, command.direction, field)
             var previousHead = head
 
+            // Move the Tails
             tails.forEachIndexed { index, tail ->
-                tails[index] = getNewTailCoord(previousHead, tail, command.direction, field)
+                tails[index] = moveTail(previousHead, tail, field)
                 previousHead = tails[index]
             }
 
-
-
             tails.last().visitedByTail = true
-//            field.print2(head, tails, start)
-//            println("")
-//            println("")
         }
     }
 
     return field.flatten().count { it.visitedByTail }
 }
 
-
-fun getNewTailCoord(head:Field, tail: Field, direction: Direction, field:List<List<Field>>): Field {
-    return when {
-        // Move the tail horizontally
-        tail.y == head.y && abs(tail.x - head.x) > 1 -> field[tail.y][(tail.x + head.x) / 2]
-        // Move the tail vertically
-        tail.x == head.x && abs(tail.y - head.y) > 1 -> field[(tail.y + head.y) / 2][tail.x]
-
-
-
-        // move diagonally
-        abs(tail.y - head.y) > 1 && abs(tail.x - head.x) > 1 -> field[(tail.y + head.y) / 2][(tail.x + head.x) / 2]
-        abs(tail.y - head.y) > 1 -> field[(tail.y + head.y) / 2][head.x]
-        abs(tail.x - head.x) > 1 -> field[head.y][(tail.x + head.x) / 2]
-        // Do not move
-        else -> return tail
-    }
-}
 
